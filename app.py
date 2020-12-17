d80f4f0652fc3eef2c173dc3862c5158de8c50df
Alba Alvaro <albaalvaro2009@hotmail.es>
2020-12-17 13:00:36 +0100

    adding changes to html for the images, css style

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 from flask_pymongo import PyMongo
+import os
 app = Flask(__name__)
+
+app.static_folder = 'static'
+app._static_folder = 'static'
+
+print(app.static_url_path)
+print(app.template_folder)
+print(app._static_folder)
+print(app.static_folder)
 
 client = MongoClient("mongodb://127.0.0.1:27017") #host uri  
 db = client["climbing-api"] #Select the database  
 
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/climbing-api"
-# mongo = PyMongo(app)
 
 # Display home
 @app.route('/', methods=["GET"])
@@ -18,41 +25,26 @@ def index():
 def index_post():
     zona = request.form['area']
     nivel = request.form['nivel']
-    popu = request.form['popu']
-    print(type(nivel))
-    if nivel=="Select a grade" and popu=="Select the popularity":
+    if nivel=="Select a grade":
         result=db[zona].find({},{"_id":0})
-        return  render_template("list.html", task = list(result))
-    elif popu=="Select the popularity":
-        result=db[zona].find({"grade": nivel}, {"_id":0, "route name":1, "grade":1}).limit(10)
-        return  render_template("list.html", task = list(result)) 
+        return render_template("list.html", task = list(result) )
+
+    elif zona=="Select an area in Spain":
+        return render_template("index.html")
+
+    elif zona!="Select an area in Spain" and nivel!="Select a grade":
+        grado=db[zona].find_one({},{"_id":0,"grade":1})
+        if (grado["grade"][1]).islower():
+            try:
+                letra=nivel[1].lower()         
+                nivel=nivel[0]+letra+nivel[2]
+            except Exception:
+                letra=nivel[1].lower()         
+                nivel=nivel[0]+letra
+        result=db[zona].find({"grade": nivel}, {"_id":0}).limit(10)
+        return render_template("list.html", task = list(result))
+
     else:
-        result=db[zona].find({"grade":nivel}, {"_id":0, "route name":1, "grade":1}).sort("ascents",-1).limit(10)
-        return  render_template("list.html", task = list(result))
-    return "Select an option"
-
-
-@app.route("/route/place")
-def routePlace():
-    # get collection
-    route = list(db[collection].find({}, {"_id":0, "route name":1, "grade":1, "sector":1}))
-    return render_template("routeplace.html", dumps(route[:5]))
-
-@app.route("/route/place/grade")
-def routeGrade():
-    # get grade, collection
-    route = list(db[collection].find({"grade": grade}, {"_id":0, "route name":1}))
-    return dumps(route[:5])
-
-@app.route("/route/place/sector/grade")
-def routeSectorGrade(grade, sector, collection):
-    route=list(db[collection].find({"grade":grade, "sector name": sector}, {"_id":0, "route name":1}))
-    return dumps(route[:10])
-
-@app.route("/route/place/sector/grade/popular")
-def PopularRoute(grade, sector, collection):
-    route=list(db[collection].find({"grade":grade, "sector name": sector}, {"_id":0, "route name":1}).sort("ascents",-1).limit(5))
-    return dumps(route)
-
-
+        print("a")
+        return render_template("index.html")
 
